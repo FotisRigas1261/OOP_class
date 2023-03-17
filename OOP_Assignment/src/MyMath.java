0182ae7616d63a1fd0638f74ac032bccf3e8e690
FotisRigas1261 <fotios-filippos.rigas@student.kuleuven.be>
2023-03-17 20:43:57 +0200

    IsSorted

--- OOP_Assignment/src/MyMath.java
+++ OOP_Assignment/src/MyMath.java
@@ -5,7 +5,7 @@ public class MyMath {
  * FORMAL DOCUMENTATION: 
  * 
  * Contractual programming vs Defensive programming.
- * @pre The number is non negative.
+ * @throws The number is non negative.
  *       |0 <=x
  * @post The result equals the ....
  * 		 | 0 <=result &&
@@ -20,5 +20,21 @@ public class MyMath {
 			r++;
 		return x;
 	}
+/**
+ * 	
+ * @throws |elements=null
+ * @post 
+ * 		|result == Instream.range(1, elements.length).allMatch(i -> elements[i-1] <= elements[i])
+ */
+		
+	static boolean IsSorted(int[] elements) {
+		if (elements==null) 
+			throw new IllegalArgumentException("elements is null");
+		for (int i=1; i<elements.length; i++)
+			if (elements[i-1]>elements[i])
+				return false;
+		return true;
+	}
+	
 	
 }
--- OOP_Assignment/src/test.java
+++ OOP_Assignment/src/test.java
@@ -12,5 +12,12 @@ class test {
 		int c=MyMath.sqrt(c2);
 		assertEquals(25,c);
 	}
-
+	
+	@Test
+	void test2() {
+		int[] elems= {12,24,45};
+		assertEquals(false,MyMath.IsSorted(elems));
+	}
+	
+	
 }
